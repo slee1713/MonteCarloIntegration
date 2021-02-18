327b0a0eb9f8454200ad52f6c7cf14c546452eed
Spencer Lee <slee216@hawk.iit.edu>
2021-02-18 17:20:28 -0600

    Initial Commit

---
+++ Shared/MonteCarloIntegrationApp.swift
@@ -0,0 +1,17 @@
+//
+//  MonteCarloIntegrationApp.swift
+//  Shared
+//
+//  Created by Spencer Lee on 2/18/21.
+//
+
+import SwiftUI
+
+@main
+struct MonteCarloIntegrationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
